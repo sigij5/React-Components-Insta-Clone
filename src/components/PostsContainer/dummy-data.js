eb7853c1eff41bd1a2a09d70a4624e6c53236fa4
Sigi Jaeckel <sigij55@gmail.com>
2020-06-09 15:50:30 -0400

    completed mvp

--- src/components/PostsContainer/dummy-data.js
+++ src/components/PostsContainer/dummy-data.js
@@ -44,7 +44,50 @@ const dummyData = [
                 text: "It that injustice"
             }
         ]
-    }
+    },
+    {
+        username: "wubbzy2000",
+        thumbnailUrl: 'https://png2.cleanpng.com/sh/52555c5c28741cb8534c7df59950c8fb/L0KzQYm3UsAzN6F8iZH0aYP2gLBuTfVud5tuReVqZH7og8S0hf1wfJpoh9C2c33sfLbCTfNtcaEyeeR9LYPkdH7sjf9rcV51htk2Y3zsgLL5lL02aWg4ftU5MUnnQbPpWL41Omg3UaYEN0G4QYi8VcA2QWQ7TKUALoDxd1==/kisspng-emoji-sadness-emoticon-smiley-clip-art-sad-emoji-png-clipart-5a73fc019d1bb8.4272949715175505936435.png',
+        imageUrl: 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        likes: 117,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "worldTraveler",
+                text: "Nice shot"
+            },
+            {
+                username: "samwiseGamgy",
+                text: "Beautiful!"
+            },
+            {
+                username: "okThen",
+                text: "cool"
+            }
+        ]
+    },
+    {
+        username: "worldTraveler",
+        thumbnailUrl: 'https://banner2.cleanpng.com/20171218/92f/smiley-png-5a3752de8a0907.5621968715135751345654.jpg',
+        imageUrl: 'https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        likes: 25456,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "wubbzy2000",
+                text: "Omg so jealous!"
+            },
+            {
+                username: "jimmytheJames",
+                text: "scary!"
+            },
+            {
+                username: "martyMcfly",
+                text: "I want to be there!"
+            }
+        ]
+    },
 ];
+
 
 export default dummyData;
